faf266ad90c37f575c1192abbf468a70d7595709
twoism <signalstatic@gmail.com>
2011-11-08 12:43:18 -0800

    updated dsl test

--- dsl_test.rb
+++ dsl_test.rb
@@ -1,15 +1,13 @@
-
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
-
 require 'phony'
 
-include Phony::Dsl
-#
 # load_url 'http://google.com'
 # set_value_of_element :with_name => 'q', :to => 'posterous'
 # click_element :with_name => 'btnG'
 # assert :HTTP_status_code_equals => 201
-# quit
+# quit_app
+
+include Phony::Dsl
 
 workflow do
   path = '/Users/twoism/dev/robocop/Login.fakeworkflow'
